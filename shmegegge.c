5bbfccaa64b37f67708e941e1308132d22d5ba0b
Yoshiya <yohno@stuy.edu>
2018-12-11 19:13:01 -0500

    Actually edit the memory, ha

--- shmegegge.c
+++ shmegegge.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+
+#define KEY 0x00DEAD00
+
 
 int main()
 {
@@ -14,7 +18,7 @@ int main()
     printf("PID: %i\n", getpid());
 
     printf("Generating shmid....\n");
-    shmid = shmget(621623, 200*sizeof(char), 0644 | IPC_CREAT);
+    shmid = shmget(KEY, 200*sizeof(char), 0644 | IPC_CREAT);
 
     printf("Attaching to memory....\n");
     if((mem = shmat(shmid, NULL, 0)) == (void *)-1)
@@ -22,8 +26,10 @@ int main()
 
     if(!*mem)
         printf("Created new shmem segment at %p\n", mem);
-    else
-        printf("Memory segment contains...\n\t%s\n", mem);
+    else {
+        printf("Found existing memory segment at %p.", mem);
+        printf("Segment contains...\n\t%s\n", mem);
+    }
 
     printf("Would you like to change the data in the segment? (y/n)\n>");
     fgets(line, 4, stdin);
@@ -31,6 +37,7 @@ int main()
     if(line[0] == 'y') {
         printf("Enter a new string....\n>");
         fgets(line, 200, stdin);
+        strcpy(mem, line);
     }
     printf("Would you like to delete the segment? (y/n)\n>");
     fgets(line, 4, stdin);
